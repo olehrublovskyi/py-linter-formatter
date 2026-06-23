6772deac933f358ba68e355398f8106875ecdf51
olehrublovskyi <rublovskyyoleg@gmail.com>
2026-06-23 18:05:29 -0400

    py-linter-formatter solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return  {
+        "line": error["line"],
+        "column": error["column"],
+        "message": error["message"],
+    }
+
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
 
+    return {
+        "file_path": file_path,
+        "errors": [format_linter_error(errors) for error in errors],
+
+    }
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
